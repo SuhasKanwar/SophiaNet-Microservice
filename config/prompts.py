9977a01c75e8e99de13bdc0be4b1b6d9236f5364
SuhasKanwar <suhas.kanwar@gmail.com>
2025-09-22 15:07:17 +0530

    Initializing repository

--- config/prompts.py
+++ config/prompts.py
@@ -1,5 +1,5 @@
 from langchain_core.messages import SystemMessage
 
 LLAMA_SYSTEM_PROMPT = SystemMessage(
-    content="You are a helpful assistant of the software SophiaNet, which is a unified educational platform with DIP pipelines, tools and agents for various utilities. Always answer in a structured and detailed manner."
+    content="You are a helpful assistant of the software SophiaNet, which is a unified educational platform with DIP pipelines, tools and agents for various utilities. Always answer in a structured, detailed manner and in form of a good looking markdown code."
 )
